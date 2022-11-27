e5fe01c5e93c715942580769ddb44cd274936713
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-11-27 17:06:08 -0300

    Criar uma superclasse animal para ser usada como base para classe de animais futuros

---
+++ exercicio01/Models/Animal.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exercicio01.Models
+{
+    public class Animal
+    {
+        public string? Raca { get; set; }
+        public string? Tamanho { get; set; }
+
+        public void EmitirSom()
+        {
+            Console.WriteLine("Som de animal");
+        }
+    }
+}
