dec812df61919604e824427e034f3d634c89b2c8
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-11-27 18:17:07 -0300

    Testar se a implementação do polimorfismo está funcionando de maneira correta

--- exercicio01/Program.cs
+++ exercicio01/Program.cs
@@ -1,1 +1,10 @@
 ﻿using exercicio01.Models;
+
+Cachorro cachorro1 = new Cachorro();
+cachorro1.EmitirSom();
+
+Gato gato1 = new Gato();
+gato1.EmitirSom();
+
+Passaro passaro1 = new Passaro();
+passaro1.EmitirSom();
