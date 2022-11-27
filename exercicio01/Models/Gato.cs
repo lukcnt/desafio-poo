5e09f1a3b1afa7da369072879f3c95e3c67c1f73
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-11-27 18:08:00 -0300

    Implementar polimorfismo no método EmitirSom mudando o som emitido para o específico animal da classe

--- exercicio01/Models/Gato.cs
+++ exercicio01/Models/Gato.cs
@@ -7,6 +7,9 @@ namespace exercicio01.Models
 {
     public class Gato : Animal
     {
-        
+        public override void EmitirSom()
+        {
+            Console.WriteLine("Miado");
+        }
     }
 }
