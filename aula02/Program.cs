4548f45c96c1f9103e7e71165c9beaba431a4602
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2022-11-29 00:13:14 -0300

    Atividade realizada durante a aula 02

---
+++ aula02/Program.cs
@@ -0,0 +1,11 @@
+﻿using aula02.Models;
+
+Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "Lucas";
+pessoa1.Idade = 28;
+pessoa1.Apresentar();
+
+Pessoa pessoa2 = new Pessoa();
+pessoa2.Nome = "Roberta";
+pessoa2.Idade = 25;
+pessoa2.Apresentar();
